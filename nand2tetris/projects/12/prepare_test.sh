4836d03e7aaa3de313d36cbd52f284c6805d19ab
Salvely <1247230776@qq.com>
2024-04-27 16:32:39 +0800

    implemented Ouput library

--- nand2tetris/projects/12/prepare_test.sh
+++ nand2tetris/projects/12/prepare_test.sh
@@ -2,9 +2,9 @@
 dirs=$(ls -l | grep ^d | cut -d" " -f 12)
 # $ ls ../../tools/OS
 # Array.vm Keyboard.vm Math.vm Memory.vm Output.vm Screen.vm String.vm Sys.vm
-for dir in $dirs; do
-    cp ../../tools/OS/* $dir
-done
+# for dir in $dirs; do
+#     cp ../../tools/OS/* $dir
+# done
 
 jack_files=$(ls | grep .jack)
 for file in $jack_files; do
